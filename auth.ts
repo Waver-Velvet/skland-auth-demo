99915d9b97ee62a5f0d585b31c0e4fc69b6fcf79
Waver Velvet <Waver_Velvet@hotmail.com>
2023-07-23 14:33:26 +0800

    feat: support granting (oauth2)

--- auth.ts
+++ auth.ts
@@ -42,6 +42,31 @@ async function getTokenByPhoneCode(phone: string, code: string): Promise<{ token
     throw new Error() // TODO
 }
 
+async function grantOAuthCode(token: string): Promise<{ code: string }> {
+    const url = 'https://as.hypergryph.com/user/oauth2/v2/grant'
+    const payload = {
+        token: token,
+
+        // no idea about following fields
+        appCode: '4ca99fa6b56cc2ba',
+        type: 0,
+    }
+    const resp = await fetch(url, { method: 'POST', body: JSON.stringify(payload) })
+    if (resp.ok) {
+        const json = await resp.json()
+        if (json.status === 0) {
+            return { code: json.data.code }
+        } else {
+            delete json.data
+            throw new Error(JSON.stringify(json))
+        }
+    } else {
+        const text = await resp.text()
+        throw new Error(`${resp.status} ${resp.statusText}: ${text}`)
+    }
+}
+
 export {
-    getTokenByPhonePassword
+    getTokenByPhonePassword,
+    grantOAuthCode
 }
