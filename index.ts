c2573c2755de8d7eb1744683f65039a2bdbdd2b7
Waver Velvet <Waver_Velvet@hotmail.com>
2023-07-23 15:06:16 +0800

    refactor: run eslint

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { getTokenByPhonePassword, grantOAuthCode } from "./src/hypergryph-auth"
-import { generateCredByCode } from "./src/skland-auth"
+import { getTokenByPhonePassword, grantOAuthCode } from './src/hypergryph-auth'
+import { generateCredByCode } from './src/skland-auth'
 
-
-async function login(phone: string, password: string): Promise<string> {
-    const { token } = await getTokenByPhonePassword(phone, password)
-    const { code } = await grantOAuthCode(token)
-    const { cred } = await generateCredByCode(code)
-    return cred
+async function login (phone: string, password: string): Promise<string> {
+  const { token } = await getTokenByPhonePassword(phone, password)
+  const { code } = await grantOAuthCode(token)
+  const { cred } = await generateCredByCode(code)
+  return cred
 }
 
 // Phone and Password of HyperGryph Account
-const phone = "<PHONE NUMBER>"
-const password = "<PASSWORD>"
+const phone = '<PHONE NUMBER>'
+const password = '<PASSWORD>'
 
 login(phone, password)
+  .then(cred => { console.log(cred) })
--- src/hypergryph-auth.ts
+++ src/hypergryph-auth.ts
@@ -1,76 +1,76 @@
-async function getTokenByPhonePassword(phone: string, password: string): Promise<{ token: string }> {
-    const payload = {
-        phone, password
+async function getTokenByPhonePassword (phone: string, password: string): Promise<{ token: string }> {
+  const payload = {
+    phone, password
+  }
+  return await fetch(
+    'https://as.hypergryph.com/user/auth/v1/token_by_phone_password',
+    {
+      method: 'POST',
+      body: JSON.stringify(payload)
     }
-    return fetch(
-        `https://as.hypergryph.com/user/auth/v1/token_by_phone_password`,
-        {
-            method: "POST",
-            body: JSON.stringify(payload),
-        }
-    )
-        .then((resp) => {
-            if (resp.ok) {
-                return resp.json()
-            } else {
-                throw new Error(`${resp.status}: ${resp.statusText}`)
-            }
-        })
-        .then(json => {
-            // const example = {
-            //     "status": 0,
-            //     "msg": "OK",
-            //     "data": {
-            //         "token": "<...>"
-            //     }
-            // }
-            if (json.status === 0) {
-                return { token: json.data.token }
-            } else {
-                // Remove sensitive data from error.
-                delete json.data
-                throw new Error(json)
-            }
-        })
+  )
+    .then(async (resp) => {
+      if (resp.ok) {
+        return await resp.json()
+      } else {
+        throw new Error(`${resp.status}: ${resp.statusText}`)
+      }
+    })
+    .then(json => {
+      // const example = {
+      //     "status": 0,
+      //     "msg": "OK",
+      //     "data": {
+      //         "token": "<...>"
+      //     }
+      // }
+      if (json.status === 0) {
+        return { token: json.data.token }
+      } else {
+        // Remove sensitive data from error.
+        delete json.data
+        throw new Error(json)
+      }
+    })
 }
 
-async function sendPhoneCode(phone: string): Promise<void> {
-    throw new Error() // TODO
+async function sendPhoneCode (phone: string): Promise<void> {
+  throw new Error() // TODO
 }
 
-async function getTokenByPhoneCode(phone: string, code: string): Promise<{ token: string }> {
-    throw new Error() // TODO
+async function getTokenByPhoneCode (phone: string, code: string): Promise<{ token: string }> {
+  throw new Error() // TODO
 }
 
-async function grantOAuthCode(token: string): Promise<{ code: string }> {
-    const url = 'https://as.hypergryph.com/user/oauth2/v2/grant'
-    const payload = {
-        token: token,
+async function grantOAuthCode (token: string): Promise<{ code: string }> {
+  const url = 'https://as.hypergryph.com/user/oauth2/v2/grant'
+  const payload = {
+    token,
 
-        // no idea about following fields
-        appCode: '4ca99fa6b56cc2ba',
-        type: 0,
-    }
-    const resp = await fetch(url, { method: 'POST', body: JSON.stringify(payload) })
-    if (resp.ok) {
-        const json = await resp.json()
-        if (json.status === 0) {
-            return { code: json.data.code }
-        } else {
-            delete json.data
-            throw new Error(JSON.stringify(json))
-        }
+    // no idea about following fields
+    appCode: '4ca99fa6b56cc2ba',
+    type: 0
+  }
+  const resp = await fetch(url, { method: 'POST', body: JSON.stringify(payload) })
+  if (resp.ok) {
+    const json = await resp.json()
+    if (json.status === 0) {
+      return { code: json.data.code }
     } else {
-        const text = await resp.text()
-        throw new Error(`${resp.status} ${resp.statusText}: ${text}`)
+      delete json.data
+      throw new Error(JSON.stringify(json))
     }
+  } else {
+    const text = await resp.text()
+    throw new Error(`${resp.status} ${resp.statusText}: ${text}`)
+  }
 }
 
 export {
-    getTokenByPhonePassword,
+  getTokenByPhonePassword,
 
-    getTokenByPhoneCode,
-    sendPhoneCode,
+  getTokenByPhoneCode,
+  sendPhoneCode,
 
-    grantOAuthCode,
+  grantOAuthCode
 }
